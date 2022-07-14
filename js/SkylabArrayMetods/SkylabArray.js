1b254d7fc9be9c6acf8edf36c7372ce3513c243e
Adrian Armesto <armestomena@gmail.com>
2022-07-15 01:41:00 +0200

    Created some method

--- js/SkylabArrayMetods/SkylabArray.js
+++ js/SkylabArrayMetods/SkylabArray.js
@@ -8,5 +8,21 @@ class SkylabArray {
       this.lenght = Number(index);
     }
   }
+
+  some(functionParameter) {
+    let conditionCounter = 0;
+    let index = 0;
+    do {
+      if (functionParameter(this[index]) === true) {
+        conditionCounter++;
+      }
+      index++;
+    } while (this[index] !== this.lenght);
+
+    if (conditionCounter > 0) {
+      return true;
+    }
+    return false;
+  }
 }
 module.exports = SkylabArray;
